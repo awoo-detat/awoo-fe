504cf9dba3428ed01495cf61d7c58ba1428afd30
Julia Kester <jkester@acvauctions.com>
2024-05-15 16:21:54 -0400

    fix linting

--- src/routes/voting/tally.js
+++ src/routes/voting/tally.js
@@ -2,39 +2,46 @@ import "@scss/tally.scss";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-export default function Tally({allUserData}) {
-  const listOfAlivePlayers = (allUserData !== undefined && allUserData.length > 0 && allUserData[0].votes)
-    ? allUserData.sort((a, b) => {
-      if (a.votes !== undefined && b.votes !== undefined) {
-        return b.votes?.length - a.votes?.length;
-      }
-      return 0;
-    })
-    : allUserData;
+export default function Tally({ allUserData }) {
+  const listOfAlivePlayers =
+    allUserData !== undefined && allUserData.length > 0 && allUserData[0].votes
+      ? allUserData.sort((a, b) => {
+          if (a.votes !== undefined && b.votes !== undefined) {
+            return (b.votes?.length || 0) - (a.votes?.length || 0);
+          }
+          return 0;
+        })
+      : allUserData;
 
-  const votingOptionsWithTallys = listOfAlivePlayers.map((item, idx) => {
+  const votingOptionsWithTallys = listOfAlivePlayers.map((item) => {
     let formattedName;
     if (item.votes?.length > 0) {
-      const combinedVoters = item.votes?.reduce((arr, vote) => {
-        arr.push(vote.voter?.name);
-        return arr;
-      }, []).join(',');
-      formattedName = `${item.name} - ${item.votes?.length} vote${item.votes?.length === 1 ? '' : 's'} from ${combinedVoters}`;
+      const combinedVoters = item.votes
+        ?.reduce((arr, vote) => {
+          arr.push(vote.voter?.name);
+          return arr;
+        }, [])
+        .join(",");
+      formattedName = `${item.name} - ${item.votes?.length} vote${item.votes?.length === 1 ? "" : "s"} from ${combinedVoters}`;
     } else {
       formattedName = `${item.name} - 0 votes`;
     }
-    return <Form.Check
-      key={`voting-choice-${idx}`}
-      type="radio"
-      label={formattedName}
-      name="voting-choice"
-    />
+    return (
+      <Form.Check
+        key={`voting-choice-${formattedName}`}
+        type="radio"
+        label={formattedName}
+        name="voting-choice"
+      />
+    );
   });
 
   return (
     <div className="tally__wrapper">
       <h3>Village Rumors</h3>
-      <p><strong>Suspected to be a werewolf:</strong></p>
+      <p>
+        <strong>Suspected to be a werewolf:</strong>
+      </p>
       {votingOptionsWithTallys}
       <Button variant="primary" type="submit">
         Vote
