f8cf8e462bf02d050b0d26915562cedc32dae6e0
Camille Kaniecki <ckaniecki@acvauctions.com>
2024-05-16 10:28:33 -0400

    should prevent you from going to game page if game isn't running

--- src/routes/desk/desk.js
+++ src/routes/desk/desk.js
@@ -2,11 +2,17 @@ import Diurnal from "@routes/diurnal/diurnal.js";
 import "@scss/desk.scss";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function Desk() {
+  const navigate = useNavigate();
   const { phase } = useSelector(({ game }) => game);
 
   const isDay = useMemo(() => phase === "day", [phase]);
+
+  if (!phase || phase < 1) {
+    navigate("/home");
+  }
 
   return (
     <div
