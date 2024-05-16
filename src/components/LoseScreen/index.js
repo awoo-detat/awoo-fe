756e984c903c71b6f1eebed1b96553472c169a65
Camille Kaniecki <86851442+camillekaniecki@users.noreply.github.com>
2024-05-16 14:11:41 -0400

    redirect to eog even if dead (#62)

--- src/components/LoseScreen/index.js
+++ src/components/LoseScreen/index.js
@@ -1,10 +1,25 @@
 import "@scss/loseScreen.scss";
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function LoseScreen({ children }) {
   const { role } = useSelector(({ user }) => user.localUser);
   const msg = useMemo(() => (!role?.alive ? "You are dead" : "Evil has won"), [role]);
+  const {
+    phase,
+    gameOverDetails,
+  } = useSelector(({ game }) => game);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (gameOverDetails) {
+      navigate("/gameover");
+    }
+    if (!phase) {
+      navigate("/");
+    }
+  }, [navigate, phase, gameOverDetails]);
 
   return (
     <div className="lose-screen">
