8a1fa7c26554b93f0f4ecbb595b8d2d72a88c96a
Julia Kester <jkester@acvauctions.com>
2024-05-16 13:06:29 -0400

    add optional chaining for .alive

--- src/components/LoseScreen/index.js
+++ src/components/LoseScreen/index.js
@@ -4,12 +4,11 @@ import { useSelector } from "react-redux";
 
 export default function LoseScreen({ children }) {
   const { role } = useSelector(({ user }) => user.localUser);
-  const msg = useMemo(() => (!role.alive ? "You are dead" : "Evil has won"), [role]);
+  const msg = useMemo(() => (!role?.alive ? "You are dead" : "Evil has won"), [role]);
 
   return (
     <div className="lose-screen">
       <h3 className="lose">{msg}</h3>
-      {/* {!role.alive ? <Tally allUserData={allUserData} /> : null} */}
       {children}
     </div>
   );
