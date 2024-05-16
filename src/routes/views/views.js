4130148679f9f8c485212ae1cfd45cbe3c4104bd
Julia Kester <jkester@acvauctions.com>
2024-05-16 15:26:59 -0400

    fix accordion so open by default

--- src/routes/views/views.js
+++ src/routes/views/views.js
@@ -36,8 +36,6 @@ export default function Views() {
     const rows = new Array(numOfViews).fill(undefined);
     console.log({ rows });
 
-    // TODO: is there any way to distinguish between night and day?
-
     return rows.map((_, ind) => {
       const currView = views[ind];
       const curId = `event-${ind}`;
@@ -70,12 +68,17 @@ export default function Views() {
     });
   }, [getMessage, numOfViews, views]);
 
-  console.log({ accordionRows });
+  const listOfAccordionIds = useMemo(
+    () => accordionRows.map(({ props }) => props.eventKey),
+    [accordionRows]
+  );
+
+  console.log({ listOfAccordionIds });
 
   return (
     <div className="views__wrapper">
       {/* <Row>{headerCols}</Row> */}
-      <Accordion defaultActiveKey="event-0" alwaysOpen>
+      <Accordion defaultActiveKey={listOfAccordionIds} alwaysOpen>
         {accordionRows}
       </Accordion>
     </div>
